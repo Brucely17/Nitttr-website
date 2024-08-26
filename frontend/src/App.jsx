4e3f3980e920f5175b43f9cc7cf1d2e6233f96eb
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-08-26 11:16:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,9 +31,9 @@ import Apr from './Annualpr/Annualpr.jsx'
 import Icc from './ICC/Internalcc.jsx'
 import AcademicStudies from './Academics/Centres/AcademicStudies/AcademicStudies.jsx';
 import Inter from './Academics/Centres/InternationalAffairs/Inter.jsx';
-// import CVOfficer from './Officer/Office.jsx'
+import CVOfficer from './Officer/Office.jsx'
 import Contact from './Contact/contact.jsx'
-import GoogleTranslate from './Googletranslate.jsx';
+import Breadcrum from './Breadcrum/Breadcrum.jsx'
 
 
 function App() {
@@ -42,7 +42,10 @@ function App() {
   return (
     <>
     <Header/>
+
+
     <BrowserRouter>
+    <Breadcrum/>
     <Routes>
       <Route path='/' element={<Home/>}/>
     <Route path='/academics/centres/resource' element={<ResourceCenter/>}/>
@@ -70,7 +73,7 @@ function App() {
     <Route path='/Academics/Centres/AcademicStudies' element={<AcademicStudies/>}/>
     <Route path= '/Departments/Civil' element={<Civil/>}/>
     <Route path='/Academics/Centres/InternationalAffairs' element={<Inter/>}/>
-    {/* <Route path='/administration/Committees & Incharge/Vigilance' element={<CVOfficer/>}/> */}
+    <Route path='/administration/Committees & Incharge/Vigilance' element={<CVOfficer/>}/>
     <Route path= '/contact' element={<Contact/>}/>
 </Routes>
   
