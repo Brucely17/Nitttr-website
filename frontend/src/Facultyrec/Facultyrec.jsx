893958f51de8ce017b5c717dc3e8baa67eece424
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-08-26 14:34:52 +0530

    Add files via upload

---
+++ frontend/src/Facultyrec/Facultyrec.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import './Facultyrec.css'; // Import the CSS file for styling
+
+const FacultyRecruitment = () => {
+  return (
+    <div className="container">
+      <h2 className="heading">Faculty Recruitment</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Department Name</th>
+            <th>Post Name</th>
+            <th>Advt. No.</th>
+            <th>Advt. Link</th>
+            <th>Corrigendum</th>
+            <th>Shortlisted Candidates</th>
+            <th>Rejected Candidates</th>
+            <th>Results</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Rural & Entrepreneurship Development</td>
+            <td>Associate Professor</td>
+            <td>11/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recruitment.php#top" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Associate%20Professor%20-%20Extension%20Corrigendum%20(1).pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Mathematics</td>
+            <td>Associate Professor</td>
+            <td>11/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recruitment.php#top" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Associate%20Professor%20-%20Extension%20Corrigendum%20(1).pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Mechanical Engineering</td>
+            <td>Professor</td>
+            <td>10/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recdoc/Educational%20Qualification%20for%20Professor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Corrigendum-To_Proferssor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Computer Science & Engineering</td>
+            <td>Professor</td>
+            <td>10/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recdoc/Educational%20Qualification%20for%20Professor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Corrigendum-To_Proferssor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Education</td>
+            <td>Professor</td>
+            <td>10/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recdoc/Educational%20Qualification%20for%20Professor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Corrigendum-To_Proferssor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Electrical Engineering</td>
+            <td>Professor</td>
+            <td>10/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recdoc/Educational%20Qualification%20for%20Professor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Corrigendum-To_Proferssor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>Educational Media and Technology</td>
+            <td>Professor</td>
+            <td>10/2023-24</td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/recdoc/Educational%20Qualification%20for%20Professor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Corrigendum-To_Proferssor.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>
+              <a href="https://www.nitttrc.ac.in/rec2023/recdoc2023/Selection&Rejection_List.pdf" target="_blank" rel="noreferrer noopener" className="pdf-link">PDF</a>
+              <img src="https://www.iiitdm.ac.in/_app/immutable/assets/new.ad5ec888.gif" alt="New Icon" className="new-icon" />
+            </td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FacultyRecruitment;
